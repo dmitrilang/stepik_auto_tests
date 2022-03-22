3fba9964ee0f593d061e9b08075231a2b1611bfc
dmitrilang <98238212+dmitrilang@users.noreply.github.com>
2022-03-22 14:40:51 +0300

    Update file1.py

---
+++ file1.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import math
+import time
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+try:
+
+
+finally:
+    time.sleep(20)
+    browser.quit()
